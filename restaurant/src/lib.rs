5d3e00f327d18a3b8d8bb25f63797c3e15102a4a
valentin994 <valentin.vareskic@gmail.com>
2023-11-28 19:34:25 +0100

    stopped at string collections

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,24 +1,11 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
+mod front_of_house;
+pub use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
     // Absolute path
     crate::front_of_house::hosting::add_to_waitlist();
-
     // Relative path
+
     front_of_house::hosting::add_to_waitlist();
 }
 
--- src/main.rs
+++ src/main.rs
@@ -132,5 +132,11 @@ fn plus_one(x: Option<i32>) -> Option<i32> {
 }
 
 fn main() {
+    let mut v = vec![1, 2, 3, 4, 5];
+
+    let first = &v[0];
+
+    v.push(6);
+
 
 }
