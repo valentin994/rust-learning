7ca45145dd54c424ffd65a838a94736118b9c508
valentin994 <valentin.vareskic@gmail.com>
2023-11-28 09:58:48 +0100

    finished 5.1

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
-fn main() {
+
+fn guess_game() {
+
     println!("Guess the number");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
@@ -31,5 +33,46 @@ fn main() {
             }
         }
     }
+}
 
+fn another_function(x: i8) {
+    println!("Another functions {x}");
+}
+
+fn takes_ownership(some_string: String) { // some_string comes into scope
+    println!("{}", some_string);
+} // Here, some_string goes out of scope and `drop` is called. The backing
+// memory is freed.
+
+fn makes_copy(some_integer: i32) { // some_integer comes into scope
+    println!("{}", some_integer);
+} // Here, some_integer goes out of scope. Nothing special happens.
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn main() {
+    let s = "hello";
+    let user1 = User {
+        active: true,
+        username: s.parse().unwrap(),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
 }
